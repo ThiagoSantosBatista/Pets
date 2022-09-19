5caa1042dc1217ed93ed93efc0b648e06807baa5
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-09-19 17:09:48 -0300

    correções, deixando responsivo e adicionando efeitos

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
-const slider = document.querySelector('.depoimentos__slider');
-const indicador = [...document.querySelectorAll('.indicador__btn')];
+const slider = document.querySelector(".depoimentos__slider");
+const indicador = [...document.querySelectorAll(".indicador__btn")];
+const mobileMenu = document.querySelector(".mobile-menu");
+const navItems = document.querySelectorAll(".header__nav-item");
 
 let currentTestimonial = 0;
 
 indicador.forEach((item, i) => {
-    item.addEventListener('click', () => {
-        indicador[currentTestimonial].classList.remove('active');
-        slider.style.marginLeft = ` -${100 * i}%`;
-        item.classList.add('active');
-        currentTestimonial = i;
-    });
+  item.addEventListener("click", () => {
+    indicador[currentTestimonial].classList.remove("active");
+    slider.style.marginLeft = ` -${100 * i}%`;
+    item.classList.add("active");
+    currentTestimonial = i;
+  });
 });
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
+  const nav = document.querySelector(".header__nav");
+  nav.classList.toggle("ativo");
+  const ativo = nav.classList.contains("ativo");
+  event.currentTarget.setAttribute("aria-expanded", ativo);
+  if (ativo) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir menu");
+  }
+}
+
+navItems.forEach((link) => {
+  link.addEventListener("click", toggleMenu);
+});
+
+mobileMenu.addEventListener("click", toggleMenu);
+mobileMenu.addEventListener("touchstart", toggleMenu);
