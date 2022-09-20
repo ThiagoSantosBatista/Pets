4c857c4677e8546662311d37e7a78a7cf304ac0c
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-09-20 12:48:49 -0300

    correção menu mobile

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ const slider = document.querySelector(".depoimentos__slider");
 const indicador = document.querySelectorAll(".indicador__btn");
 const mobileMenu = document.querySelector(".mobile-menu");
 const navItems = document.querySelectorAll(".header__nav-item");
-const nav = document.querySelector(".header__nav");
-const ativo = nav.classList.contains("ativo");
 
 let currentTestimonial = 0;
 
@@ -20,19 +18,21 @@ function toggleMenu(event) {
   if (event.type === "touchstart") {
     event.preventDefault();
   }
+  const nav = document.querySelector(".header__nav");
   nav.classList.toggle("ativo");
+  const ativo = nav.classList.contains("ativo");
   event.currentTarget.setAttribute("aria-expanded", ativo);
   if (ativo) {
     event.currentTarget.setAttribute("aria-label", "Fechar menu");
-    navItems.forEach((link) => {
-      link.addEventListener("click", () => {
-        nav.classList.toggle("ativo");
-      });
-    });
   } else {
     event.currentTarget.setAttribute("aria-label", "Abrir menu");
   }
 }
-
+navItems.forEach((link) => {
+  const nav = document.querySelector(".header__nav");
+  link.addEventListener("click", () => {
+    nav.classList.toggle("ativo");
+  });
+});
 mobileMenu.addEventListener("click", toggleMenu);
 mobileMenu.addEventListener("touchstart", toggleMenu);
